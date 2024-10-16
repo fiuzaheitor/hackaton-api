87bac79a227c7b4ce41ee06d5665e8978dc39082
Bruno Lustoza <102392383+bruno-lustoza@users.noreply.github.com>
2024-10-16 03:42:22 -0300

    Modules - consultation

---
+++ src/graphql/modules/consultation/resolvers.ts
@@ -0,0 +1,64 @@
+import { GraphQLError } from "graphql";
+import { Users } from "../../../models/User";
+import auth from "../../../util/auth";
+import { Consultations } from "../../../models/Consultation";
+
+const Consultation = {
+  createdBy: async (consultation: any) => {
+    try {
+      const user = await Users.findById(consultation.createdBy);
+      if (user) {
+        return user;
+      }
+    } catch (err: any) {
+      throw new GraphQLError(err.message);
+    }
+  },
+  updatedBy: async (consultation: any) => {
+    try {
+      const user = await Users.findById(consultation.updatedBy);
+      if (user) {
+        return user;
+      }
+    } catch (err: any) {
+      throw new GraphQLError(err.message);
+    }
+  },
+};
+
+const Query = {
+  async consultation(_: any, { id }: { id: string }) {
+    try {
+      const consultation = await Consultations.findById(id);
+        if (consultation) {
+            return consultation;
+        } else {
+            throw new GraphQLError("Consultation não existe");
+        }
+    }
+    catch (err: any) {
+        throw new GraphQLError(err.message);
+    }
+    },
+    async consultations() {
+        try {
+            return await Consultations.find({});
+        } catch (err: any) {
+            throw new GraphQLError(err.message);
+        }
+    },
+};
+
+const Mutation = {
+    async createConsultation(_: any, { data }: { data: any }, context: any) {
+        try {
+            const userAuth = await auth(context);
+            const newConsultation = new  Consultations({ ...data, createdBy: typeof userAuth === "string" ? userAuth : userAuth.id,});
+            return await newConsultation.save();
+        } catch (err: any) {
+            throw new GraphQLError(err.message);
+        }
+    },
+};
+
+export default { Consultation, Query, Mutation };
--- src/models/Consultation.ts
+++ src/models/Consultation.ts
@@ -1,12 +1,13 @@
 import { model, Schema } from 'mongoose';
 
-const GestationSchema = new Schema({
-  mother: { type: Schema.Types.ObjectId, ref: 'User' },
-  duration: Number,
+const ConsultationSchema = new Schema({
+  date: Number,
+  month: Number,
+  hour: Number,
   createdAt: { type: Number, default: Date.now },
   updatedAt: { type: Number, default: Date.now },
   createdBy: { type: Schema.Types.ObjectId, ref: 'User' },
   updatedBy: { type: Schema.Types.ObjectId, ref: 'User' },
 });
 
-export const Gestations = model('Gestation', GestationSchema);
+export const Consultations = model('Consultation', ConsultationSchema);
